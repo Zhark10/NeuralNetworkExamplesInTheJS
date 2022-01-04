d492963241e2de11c70d1ce4d8e38478f966f3dc
Zhark10 <verygoodman18@yandex.ru>
2022-01-05 01:00:54 +0300

    add main func

--- index.js
+++ index.js
@@ -20,3 +20,29 @@ class Neuron {
     this.weigth += correction
   }
 }
+
+function main() {
+  const trainData = {
+    usd: 75.32,
+    rub: 1
+  }
+
+  const neuron = new Neuron()
+
+  let i = 0
+  do {
+    i++;
+    neuron.train(trainData.rub, trainData.usd)
+    if (i % 10000 === 0) {
+      console.error(`Итерация ${i}. Ошибка: ${neuron.lastError}`)
+    }
+  } while (Math.abs(neuron.lastError) > neuron.ratioToTrain)
+
+  const cases = [23, 6, 2, 1, 1, 587]
+
+  cases.forEach(value => {
+    console.log(`${value} руб. - это ${neuron.convert(value)} дол.`)
+  })
+}
+
+main()
