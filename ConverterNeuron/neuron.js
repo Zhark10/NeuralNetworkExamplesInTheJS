bc4330c6ad4500c7ae01809c13fa45257e930104
Zhark10 <verygoodman18@yandex.ru>
2022-01-06 01:06:09 +0300

    converter neuron class

---
+++ ConverterNeuron/neuron.js
@@ -0,0 +1,22 @@
+module.exports = class Neuron {
+  constructor() {
+    this.weigth = 50
+    this.lastError = 0
+    this.ratioToTrain = 0.00001
+  }
+
+  convert(inputValue) {
+    return inputValue * this.weigth
+  }
+
+  unconvert(outputValue) {
+    return outputValue / this.weigth
+  }
+
+  train(inputValue, expectedResult) {
+    const actualResult = inputValue * this.weigth
+    this.lastError = expectedResult - actualResult
+    const correction = (this.lastError / actualResult) * this.ratioToTrain
+    this.weigth += correction
+  }
+}
